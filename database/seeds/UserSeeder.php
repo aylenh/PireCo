10cca4a474a61997ff849155a6ed73bb55ed943d
Max <themaxhs@gmail.com>
2021-09-29 14:50:05 -0500

    se arreglaron algunos errores en las cajas

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -14,6 +14,7 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+
         DB::table('users')->insert([
             'name' => 'AlbertoDev',
             'email' => 'alberto@dev.com',
