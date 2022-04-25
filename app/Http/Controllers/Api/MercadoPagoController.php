2a1f5a19e4d318cc75b8a957813bf1d8ffcf418a
Diohandres Hernandez <diohandres1703@hotmail.com>
2022-04-25 09:56:57 -0400

    Mercado Pago

--- app/Http/Controllers/Api/MercadoPagoController.php
+++ app/Http/Controllers/Api/MercadoPagoController.php
@@ -12,20 +12,20 @@ class MercadoPagoController extends Controller
         \MercadoPago\SDK::setAccessToken(env('MP_ACCESS_TOKEN'));
     }
 
-    public function notification(){
+    public function notification($request){
         \MercadoPago\SDK::setAccessToken("ENV_ACCESS_TOKEN");
         switch($_POST["type"]) {
             case "payment":
-                $payment = \MercadoPago\Payment::find_by_id($_POST["data"]["id"]);
+                $payment = \MercadoPago\Payment::find_by_id($request->data->id);
                 break;
             case "plan":
-                $plan = \MercadoPago\Plan::find_by_id($_POST["data"]["id"]);
+                $plan = \MercadoPago\Plan::find_by_id($request->data->id);
                 break;
             case "subscription":
-                $plan = \MercadoPago\Subscription::find_by_id($_POST["data"]["id"]);
+                $plan = \MercadoPago\Subscription::find_by_id($request->data->id);
                 break;
             case "invoice":
-                $plan = \MercadoPago\Invoice::find_by_id($_POST["data"]["id"]);
+                $plan = \MercadoPago\Invoice::find_by_id($request->data->id);
                 break;
             case "point_integration_wh":
                 // $_POST contiene la informaciòn relacionada a la notificaciòn.
