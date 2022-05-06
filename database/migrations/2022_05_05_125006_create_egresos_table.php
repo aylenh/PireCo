5fe5dc4a9f0f566df27ef772adaab40439a99e4c
Diohandres Hernandez <diohandres1703@hotmail.com>
2022-05-06 10:09:56 -0400

    Imagen de Login

---
+++ database/migrations/2022_05_05_125006_create_egresos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEgresosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('egresos', function (Blueprint $table) {
+            $table->id();
+            $table->string('cliente_distribuidor',50)->nullable();
+            $table->dateTime('fecha')->nullable();
+            $table->double('monto')->nullable();
+            $table->string('nota',50)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('egresos');
+    }
+}
