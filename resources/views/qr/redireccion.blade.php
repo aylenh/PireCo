1d78371c19a57a0d4236b911dad2ca2b016f485f
paulacaicedo <paulacaicedo0322@gmail.com>
2022-07-03 22:27:20 -0500

    datos ultimos

--- resources/views/qr/redireccion.blade.php
+++ resources/views/qr/redireccion.blade.php
@@ -8,16 +8,17 @@
 // }
 
 var userAgent = navigator.userAgent || navigator.vendor || window.opera;
-	if (/windows phone/i.test(userAgent)) {
-		// location.replace("https://goa-ti.com/");
-        alert('no hay');
- 	}
+	// if (/windows phone/i.test(userAgent)) {
+	// 	// location.replace("https://goa-ti.com/");
+    //     alert('no hay');
+ 	// }
 
 	if (/android/i.test(userAgent)) {
 		location.replace("https://play.google.com/store/apps/details?id=com.whatsapp&hl=es_CO&gl=US");
 	}
 
 	if ((navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPod/i))) {
-		location.replace("https://goa-ti.com");
+		location.replace("https://apps.apple.com/es/app/whatsapp-messenger/id310633997");
 	}
 </script>
+{{-- redireccionamiento app  --}}
