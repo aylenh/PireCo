9a906549e383bd392311573991c9837d9e753377
paula-caicedo3C <118369269+paula-caicedo3C@users.noreply.github.com>
2022-11-24 13:08:28 -0500

    maps

--- resources/views/distribuidores/edit-distribuidor-modal.blade.php
+++ resources/views/distribuidores/edit-distribuidor-modal.blade.php
@@ -12,30 +12,43 @@
                 <form action="{{route('update.distribuidor')}}" method="POST" enctype="multipart/form-data" id="update_form">
                     @csrf
                     <input type="hidden" name="pid">
-                    <div class="form-group">
+                    <div class="mb-3 col-lg-6 col-sm-12">
                         <label for="">Nombre Local</label>
                         <input type="text" class="form-control" name="distribuidor_local"
                             placeholder="Ingresa el nombre del local nuevo">
                         <span class="text-danger error-text distribuidor_local_error"></span>
                     </div>
-                    <div class="form-group">
+                    <div class="mb-3 col-lg-6 col-sm-12">
                         <label for="">Correo</label>
                         <input type="email" class="form-control" name="distribuidor_correo"
                             placeholder="Ingresa el correo nuevo">
                         <span class="text-danger error-text distribuidor_correo_error"></span>
                     </div>
-                    <div class="form-group">
+                    <div class="mb-3 col-lg-6 col-sm-12">
                         <label for="">Numero de contacto</label>
                         <input type="number" class="form-control" name="distribuidor_contacto"
                             placeholder="Ingresa el numero de contacto nuevo">
                         <span class="text-danger error-text distribuidor_contacto_error"></span>
                     </div>
-                    <div class="form-group">
+                    <div class="mb-3 col-lg-6 col-sm-12">
                         <label for="">Ubicacion</label>
                         <input id="updateLocation" type="text" class="form-control" name="distribuidor_ubicacion"
                             placeholder="Ingresa la ubicacion nueva">
                         <span class="text-danger error-text distribuidor_ubicacion_error"></span>
                     </div>
+                    <div class="mb-3 col-lg-6 col-sm-12">
+                        <label for="">Latitud</label>
+                        <input id="updateLat" type="text" class="form-control" name="updateLat"
+                            placeholder="Ingresa la ubicacion nueva">
+                        <span class="text-danger error-text distribuidor_ubicacion_error"></span>
+                    </div>
+                    <div class="mb-3 col-lg-6 col-sm-12">
+                        <label for="">Longitud</label>
+                        <input id="updateLng" type="text" class="form-control" name="updateLng"
+                            placeholder="Ingresa la ubicacion nueva">
+                        <span class="text-danger error-text distribuidor_ubicacion_error"></span>
+                    </div>
+
                     <div id="updateMap" style="width: 400px; height: 300px;"></div>
                     <br>
                    <!-- <div class="form-group">
