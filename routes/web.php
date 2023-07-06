165faa726a570c502f6b7765457fbf746136bbca
paula-caicedo3C <118369269+paula-caicedo3C@users.noreply.github.com>
2023-07-06 10:33:58 -0500

    datos6

--- routes/web.php
+++ routes/web.php
@@ -66,7 +66,7 @@ Route::post('/borrarProductos', 'ProductoController@borrarProductosCheck')->name
 Route::post('/borrarProducto', 'ProductoController@borrarProducto')->name('borrar.producto');
 
 // QR 
-Route::view('/DescargarApp', 'qr.redireccion');
+Route::view('/Instagram.', 'qr.redireccion');
 
 //RUTAS ENCARGOS
 
